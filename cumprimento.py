6dd10e755c167958d3aa98de78339062ac6d308e
Juliana Bergamini <98159983+JulianaBergamini@users.noreply.github.com>
2022-01-26 18:37:03 -0300

    Changes

---
+++ cumprimento.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+nome = str(raw_input('Qual é o seu nome? '))
+if nome == 'João':
+    print ('Prazer em te conhecer, João! Que nome lindo você tem!')
+else:
+    print ('Prazer em te conhecer!')
+print('Bom dia, {}!'.format(nome))
+
